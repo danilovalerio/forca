16e3e727b8d61a230767b5cc339a28deba4648a7
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-11-01 18:08:43 -0300

    Base commit

---
+++ android/app/src/main/kotlin/projetos/danilo/forca/forca/MainActivity.kt
@@ -0,0 +1,6 @@
+package projetos.danilo.forca.forca
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ForcaApp());
+}
+
+class ForcaApp extends StatelessWidget {
+  const ForcaApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Forca Game',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        backgroundColor: Colors.green,
+      ),
+      home: const ForcaHomePage(),
+    );
+  }
+}
+
+class ForcaHomePage extends StatelessWidget {
+  const ForcaHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Theme.of(context).backgroundColor,
+      ),
+    );
+  }
+}
