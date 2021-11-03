daa9ce67ef6f78b0f42e24ae00cd54decb5f6c1a
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-11-03 11:52:05 -0300

    adicao de um linearProgressIndicator

--- lib/routes/splash_screen_route.dart
+++ lib/routes/splash_screen_route.dart
@@ -16,12 +16,32 @@ class _SplashScreenRouteState extends State<SplashScreenRoute> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CircularImageWidget(
-        imgProvider: AssetImage(
-          'assets/images/splashscreen.jpg',
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CircularImageWidget(
+          imgProvider: AssetImage(
+            'assets/images/splashscreen.jpg',
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25.0, bottom: 25),
+          child: Text(
+            'Carregando...',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 100, right: 100),
+          child: LinearProgressIndicator(
+            backgroundColor: Colors.blue[200],
+            valueColor: AlwaysStoppedAnimation(Colors.blue[900]),
+          ),
+        ),
+      ],
     );
   }
 }
